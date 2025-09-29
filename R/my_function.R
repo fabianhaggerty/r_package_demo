3cb08a488009ab52dd3b3250da6ec427953d4a47
Haggerty <F.Haggerty@campus.lmu.de>
2025-09-29 16:13:09 +0200

    Second edit

--- R/my_function.R
+++ R/my_function.R
@@ -1,4 +1,15 @@
 
+#' @title Print text x times
+#'
+#' @description
+#' `print_string_n_times()` prints text as often as specified.
+#'
+#' @param text A string to be printed.
+#'
+#' @param n An integer specifying how many times to print the text.
+#'
+#' @export
+
 print_string_n_times <- function(text, n) {
   for (i in 1:n) {
     print(text)
